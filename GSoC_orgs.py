d5d9bb21b2dd159f84fd04d1b66d2aa07dd6f26e
Anveshan Lal <anveshanrx8@gmail.com>
2019-02-10 12:56:59 +0530

    -created a dict table to store data of each column in seperate lists used pandas to export to csv

--- GSoC_orgs.py
+++ GSoC_orgs.py
@@ -1,31 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
+from pandas import DataFrame
+from array import *
 
-def get_links():
-        url = 'https://summerofcode.withgoogle.com/archive/2018/organizations/'
 
-        r = requests.get(url)
+url = 'https://summerofcode.withgoogle.com/archive/2018/organizations/'
 
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+rows = soup.select('section div ul li')
+
+link_list = []
+
+for row in rows:
+        abc = 'https://summerofcode.withgoogle.com' + row.select_one('a')['href']
+        link_list.append(abc)
+
+OrgName = []
+Contactlink = []
+lisat = {}
+i = 0       
+j = 0
+for org_url in link_list:
+        r = requests.get(org_url)
         soup = BeautifulSoup(r.content, 'html.parser')
+        org = soup.find('div', class_="banner__text")              
+        OrgName.append(org.h3.text)
+        technologies = soup.find_all('li', class_="organization__tag--technology")
+        # for technology in technologies:
+        #         lisat[i,j] = technology.text
+        #         j = j + 1
+        # i = i + 1
+        irc = soup.select_one(".org__meta-button")['href']
+        Contactlink.append(irc)
 
-        rows = soup.select('section div ul li')
-        link_list = []
+table = {'Org' : OrgName, 'Link' : link_list , 'Contact' : Contactlink}
 
-        for row in rows:
-                abc = 'https://summerofcode.withgoogle.com' + row.select_one('a')['href']
-                link_list.append(abc)
-                
-        return link_list
-
-def get_org(link_list):       
-        for org_url in link_list:
-                r = requests.get(org_url)
-                soup = BeautifulSoup(r.content, 'html.parser')
-                org = soup.find('div', class_="banner__text")
-                print("\n",org.h3.text,"\n")
-                technologies = soup.find_all('li', class_="organization__tag--technology")
-                for technology in technologies:
-                        print(technology.text)
-
-links = get_links()
-get_org(links)
+df = DataFrame(table)
+export_csv = df.to_csv(r'Resuls.csv')
+        
+print(r'Done!')
