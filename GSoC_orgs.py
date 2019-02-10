7a7915847ad91d684c3c9bfee4869d000f946f9c
Anveshan Lal <anveshanrx8@gmail.com>
2019-02-10 16:22:56 +0530

    added technologies tab :D

--- GSoC_orgs.py
+++ GSoC_orgs.py
@@ -20,25 +20,25 @@ for row in rows:
 
 OrgName = []
 Contactlink = []
-lisat = {}
-i = 0       
-j = 0
+techlist = []
+
 for org_url in link_list:
+        lisat = []
         r = requests.get(org_url)
         soup = BeautifulSoup(r.content, 'html.parser')
         org = soup.find('div', class_="banner__text")              
         OrgName.append(f"=HYPERLINK(\"{org_url}\",\"{org.h3.text}\")")
         technologies = soup.find_all('li', class_="organization__tag--technology")
-        # for technology in technologies:
-        #         lisat[i,j] = technology.text
-        #         j = j + 1
-        # i = i + 1
+        for technology in technologies:
+                lisat.append(technology.text)
+        mys = ', '.join(lisat)
+        techlist.append(mys)
         irc = soup.select_one(".org__meta-button")['href']
         Contactlink.append(irc)
 
-table = {'Org' : OrgName , 'Contact' : Contactlink}
+table = {'Org' : OrgName , 'Technologies' : techlist , 'Contact' : Contactlink}
 
 df = DataFrame(table)
-export_csv = df.to_csv(r'Resuls.csv')
+export_csv = df.to_csv(r'GSoC-Orgs.csv')
         
 print(r'Done!')
