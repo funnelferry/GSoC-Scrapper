a822041106e68b5fc4707245981c6bea090b17ea
Anveshan Lal <anveshanrx8@gmail.com>
2019-02-13 14:43:35 +0530

    All features added and all issues resolved, yaygit add .!

--- GSoC_orgs.py
+++ GSoC_orgs.py
@@ -20,13 +20,14 @@ OrgName = []
 Contactlink = []
 techlist = []
 slots = []
+ideas = []
 
 for org_url in link_list:
         lisat = []
         r = requests.get(org_url)
         soup = BeautifulSoup(r.text, 'html.parser')
         org = soup.find('div', class_="banner__text")              
-        # OrgName.append(f"=HYPERLINK(\"{org_url}\",\"{org.h3.text}\")")
+        OrgName.append(f"=HYPERLINK(\"{org_url}\",\"{org.h3.text}\")")
         technologies = soup.find_all('li', class_="organization__tag--technology")
         for technology in technologies:
                 lisat.append(technology.text)
@@ -35,13 +36,15 @@ for org_url in link_list:
         irc = soup.select_one(".org__meta-button")['href']
         Contactlink.append(irc)
         projects = soup.find('ul', class_="project-list-container")
-        slots = projects.findChildren('li')
-        slots.append(len(slots))
-        ideas = soup.select_one(".org__button-container")
-        print(ideas)
+        slot = projects.findChildren('li')
+        slots.append(len(slot))
+        idea = soup.select_one(".org__button-container md-button")['href']
+        # idea = ideas[0].select_one('md-button')['href']
+        ideas.append(idea)
+        
         
 
-table = {'Org' : OrgName , 'Technologies' : techlist , 'Contact' : Contactlink, 'Slots' : slots}
+table = {'Org' : OrgName , 'Technologies' : techlist , 'Slots' : slots , 'Ideas Page' : ideas , 'Contact' : Contactlink}
 
 df = DataFrame(table)
 export_csv = df.to_csv(r'GSoC-Orgs.csv')
